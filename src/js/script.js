8317f9097bc001be606ec411f6f3fcae195f0d9b
daniyarrrrrrrr <daniyar20070902@gmail.com>
2022-05-19 10:08:53 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        // nav:true,
+        // dots:true,
+        autoplay:true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+
+    });
+});
